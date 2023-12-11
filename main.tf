0115bc51e13358b2c05f2caf9dc7fb726d62ea49
Marco Conti <marcoaurelioconti@yahoo.com.br>
2023-12-11 09:24:06 -0300

    Primeiro commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.6.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "treino_terraform_bucket" {
+  bucket = "gha-terraform-bucket-mac-${var.environment}-20231208-v2"
+  tags   = var.tags
+}
